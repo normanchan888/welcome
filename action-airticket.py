0f3a7134d270fbae8ad24a1d304e939473343b78
normanchan888 <normanchan888@yahoo.com>
2018-11-22 14:25:44 +0800

    Update action-airticket.py

--- action-airticket.py
+++ action-airticket.py
@@ -2,7 +2,7 @@
 from hermes_python.hermes import Hermes
 
 def intent_received(hermes, intent_message):
-    print('Intent {}'.format(intent_message.intent))
+    print('Intent {}'.format(intent_message.intent.intent_name))
 
     for (slot_value, slot) in intent_message.slots.items():
         print('Slot {} -> \n\tRaw: {} \tValue: {}'.format(slot_value, slot[0].raw_value, slot[0].slot_value.value.value))
