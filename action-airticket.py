25b256b9b19a4908016463ed2fc26e6e00d34bc1
normanchan888 <normanchan888@yahoo.com>
2018-11-22 12:14:14 +0800

    Update action-airticket.py

--- action-airticket.py
+++ action-airticket.py
@@ -1,60 +1,99 @@
 #!/usr/bin/env python2
-# -*-: coding utf-8 -*-
+# -*- coding: utf-8 -*-
 
 from hermes_python.hermes import Hermes
-import json
+import time
+import datetime
+import string
+import os
+
 
 
 MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
+def intents_callback(hermes, intentMessage) : 
+ 
+    if intentMessage.intent.intent_name == 'normanchan888:book_flight' :
+            
+            print("User is booking a flight")
+            if intent_message.slots.departcity:
+                depart_city = intent_message.slots.departcity.first().value
+            if intent_message.slots.arrivecity:
+                arrive_city = intent_message.slots.arrivecity.first().value
 
-INTENT_BOOK_FLIGHT = "normanchan888:book_flight"
-INTENT_AIRPORT_SELECTION = "normanchan888:airport_selection"
+            hermes.publish_continue_session(intent_message.session_id, "which airport", INTENT_AIRPORT_SELECTION)
+                                           
+      
+    elif intentMessage.intent.intent_name == 'louisros:signe' :
+            s = ""
+            m = ""
+            t = ""
+            if len(intentMessage.slots.s):
+                  s = intentMessage.slots.s.first().value 
+            if len(intentMessage.slots.m):     
+                  m = intentMessage.slots.m.first().value
+            if len(intentMessage.slots.t):
+                  t = intentMessage.slots.t.first().value
+            if t != 'fin' :
+                  if m != "" :
+                        s = m[0]
+                  if t == 'grand' :
+                        s = string.upper(s)                     
+                  n = n + s  
+                  
+                  hermes.publish_continue_session(intentMessage.session_id,"suivant",["louisros:signe","louisros:keyOK"])
+            else:
+                  global ssid
+                  global key
+                  if phase == 0 :
+                        ssid = n
+                        n = ""
+                        phase = 1                                                             
+                        hermes.publish_continue_session(intentMessage.session_id,"Ce nom de S S I D est-il correct?",["louisros:ssidOK","louisros:changeSSID"])
 
+                  else:
+                        key = n
+                        hermes.publish_continue_session(intentMessage.session_id,"Validez vous cette clé?",["louisros:keyOK","louisros:changeSSID"])
+                        
+    elif intentMessage.intent.intent_name == 'louisros:ssidOK' :
+            ok = intentMessage.slots.ok.first().value 
+            if ok != "oui":
+                  hermes.publish_end_session(intentMessage.session_id, "mise à jour abandonnée")
+            else:
+                  hermes.publish_continue_session(intentMessage.session_id,"nouvelle clé premier caractère",["louisros:signe"])
+      
+    elif intentMessage.intent.intent_name == 'louisros:keyOK' :   
+            ok = intentMessage.slots.ok.first().value 
+            if ok != "oui":
+                  hermes.publish_end_session(intentMessage.session_id, "mise à jour abandonnée")
+            else:     
+                
+       
+                  ssid = '\"' + ssid + '\"'
+                  key = '\"' + key + '\"'
+                  r = '\n\nnetwork = {\nssid=' + ssid + '\npsk=' + key + '\n}'
+          
+                  """fv = open("/etc/wpa_supplicant/wpa_supplicant.conf","r")
+                  s = fv.read()
+                  fv.close()
+                  """
+                  fv =  open("/var/lib/snips/skills/wpa_supplicant.conf","w")
+                  #fv.write(s + r)
+                  fv.write(r)
+                  fv.close()
+                
+                  os.system("sudo cp /var/lib/snips/skills/wpa_suplicant.conf /etc/wpa_supplicant")
+                 
 
-def user_book_flight(hermes, intent_message):
-    print("User is booking a flight")
+                  hermes.publish_end_session(intentMessage.session_id, "mise à jour effectuée")
+      
+    elif intentMessage.intent.intent_name == 'louisros:changeKEY' :
+            hermes.publish_continue_session(intentMessage.session_id,"premier caractère",["louisros:signe","louisros:changeSSID"])
+      
 
-    if intent_message.slots.departcity:
-        depart_city = intent_message.slots.departcity.first().value
-    if intent_message.slots.arrivecity:
-        arrive_city = intent_message.slots.arrivecity.first().value
-
-    hermes.publish_continue_session(intent_message.session_id, "which airport", INTENT_AIRPORT_SELECTION)
-
-
-def user_airport_selection(hermes, intent_message):
-    print("User is selecting an airport")
-
-    session_id = intent_message.session_id
-
-    if intent_message.slots.airport:
-        airport = intent_message.slots.airport.first().value
-
-    hermes.publish_end_session(session_id, "Bye")
-
-
-def session_started(hermes, session_started_message):
-    print("Session Started")
-
-    print("sessionID: {}".format(session_started_message.session_id))
-    print("session site ID: {}".format(session_started_message.site_id))
-
-    session_id = session_started_message.session_id
-
-
-def session_ended(hermes, session_ended_message):
-    print("Session Ended")
-    session_id = session_ended_message.session_id
-    session_site_id = session_ended_message.site_id
-
-
-with Hermes(MQTT_ADDR) as h:
-
-    h.subscribe_intent(INTENT_BOOK_FLIGHT, user_book_flight) \
-        .subscribe_intent(INTENT_AIRPORT_SELECTION, user_airport_selection) \
-        .subscribe_session_ended(session_ended) \
-        .subscribe_session_started(session_started) \
-        .start()
+if __name__ == "__main__":
+    with Hermes(MQTT_ADDR) as h:           
+            h.subscribe_intents(intents_callback).start()
+        
