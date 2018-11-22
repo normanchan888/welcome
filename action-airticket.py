73533dc1543244894d791cfda7a53a76e8354e4f
normanchan888 <normanchan888@yahoo.com>
2018-11-22 13:40:51 +0800

    Update action-airticket.py

--- action-airticket.py
+++ action-airticket.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python2
 from hermes_python.hermes import Hermes
-​
+
 def intent_received(hermes, intent_message):
     print('Intent {}'.format(intent_message.intent))
-​
+
     for (slot_value, slot) in intent_message.slots.items():
         print('Slot {} -> \n\tRaw: {} \tValue: {}'.format(slot_value, slot[0].raw_value, slot[0].slot_value.value.value))
-​
+
     hermes.publish_end_session(intent_message.session_id, None)
-​
-with Hermes('10.81.0.223:1883') as h:
+
+with Hermes('raspberrypi.local:1883') as h:
     h.subscribe_intents(intent_received).start()
